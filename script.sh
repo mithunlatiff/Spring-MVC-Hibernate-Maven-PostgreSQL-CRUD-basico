c91608780da36377b77285ece499fdf7e41b7a92
Latiff, Mithun <Mithun.Latiff@sicpa.com>
2021-04-21 00:18:06 +0800

    jj

--- script.sh
+++ script.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cd /tmp
-HOOK_RETRIES=200
+HOOK_RETRIES=200000000
+set PGPASSWORD=$POSTGRESQL_PASSWORD
 curl -s  https://raw.githubusercontent.com/dsubires/Spring-MVC-Hibernate-Maven-PostgreSQL-CRUD-basico/master/postgreSQL.sql -o /tmp/postgreSQL.sql
 while [ "$HOOK_RETRIES" != 0 ]; do
     echo "Checking if db is up"
-    if psql -lqt -h db -U $POSTGRESQL_USER  -p 5432 -d $POSTGRESQL_DATABASE | cut -d \| -f 1 | grep -qw $POSTGRES_DATABASE; then 
+    if psql  -h db -U $POSTGRESQL_USER  -p 5432 -d $POSTGRESQL_DATABASE; then 
     echo "Database is up"
-    set PGPASSWORD=$POSTGRESQL_PASSWORD
+    
     psql -h db -U $POSTGRESQL_USER  -p 5432 -d $POSTGRESQL_DATABASE < /tmp/postgreSQL.sql
     else
     echo "Db down"
